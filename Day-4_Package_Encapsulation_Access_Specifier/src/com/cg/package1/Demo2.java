9d562b63d7aed8076261f7f7f9c9df1103d5a605
Admin <Admin@DESKTOP-IJ4V5DK>
2022-08-04 19:50:12 +0530

    First Commit

---
+++ Day-4_Package_Encapsulation_Access_Specifier/src/com/cg/package1/Demo2.java
@@ -0,0 +1,14 @@
+package com.cg.package1;
+//package name and .class name of other package
+import com.cg.package2.Demo;
+
+public class Demo2 {
+// one main method
+	public static void main(String[] args) {
+
+		Demo d = new Demo();
+		d.display(20,"Kumar");
+		
+	}
+
+}
---
+++ Day-4_Package_Encapsulation_Access_Specifier/src/com/cg/package2/Demo.java
@@ -0,0 +1,17 @@
+package com.cg.package2;
+
+public class Demo {
+ 
+	 int age;
+	 String name;
+	
+	
+	//should enter public in method if accessing in another package
+	
+  public void display(int age, String name)
+	{
+		System.out.println("Your Age is:"+age);
+		System.out.println("Your Name is:"+name);
+		
+	}
+}
