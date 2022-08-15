9d0f1b68663027b1b69a43c24f4b37cda3166ad4
Admin <Admin@DESKTOP-IJ4V5DK>
2022-08-15 19:14:37 +0530

    First Commit

---
+++ Assignment3/src/com/cg/string/StringBuffer.java
@@ -0,0 +1,19 @@
+package com.cg.string;
+
+public class StringBuffer {
+
+		// TODO Auto-generated method stub
+		public StringBuffer(String string) {
+
+	}
+		public static void main(String[] args) {
+			StringBuffer s=new StringBuffer("My name is Khan");
+			s.reverse();
+			System.out.println(s);
+		
+}
+		private void reverse() {
+			// TODO Auto-generated method stub
+			
+		}
+}
