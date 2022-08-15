07a8ec3df19dc9e1ae7272e7a946fcc8ba46bfe8
Admin <Admin@DESKTOP-IJ4V5DK>
2022-08-15 19:13:35 +0530

    First Commit

---
+++ Day-10_Array/src/com/cg/array/ArrayDemo.java
@@ -0,0 +1,15 @@
+package com.cg.array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+
+		int arr[]=  new int[]{11,12,13,14};
+		System.out.println("The Array index value:"+arr[1]);
+		System.out.println("The Array index value:"+arr[5]);
+		
+		
+
+	}
+
+}
---
+++ Day-10_Array/src/com/cg/array/TwoDimensionalArray.java
@@ -0,0 +1,16 @@
+package com.cg.array;
+
+import java.util.Arrays;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]= {{11,5,3},{5,4,8}};
+		System.out.println(arr[0][2]);
+		System.out.println(arr[1][1]);
+		
+
+	}
+
+}
