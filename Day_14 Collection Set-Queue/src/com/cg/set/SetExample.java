2b3e3059081536b516377ed9b4f0f4f6d672863a
Admin <Admin@DESKTOP-IJ4V5DK>
2022-08-25 19:41:24 +0530

    First Commit

---
+++ Day_14 Collection Set-Queue/src/com/cg/set/SetExample.java
@@ -0,0 +1,19 @@
+package com.cg.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+	public static void main(String[] args) {
+		Set <Object> s=new HashSet<>();
+		s.add(45);
+		s.add(45);
+		s.add(78);
+		s.add(23);
+		s.add(18);
+		s.add("ssd");
+		System.out.println(s);
+
+}
+
+}
---
+++ Day_14 Collection Set-Queue/src/com/cg/set/SortSetExample.java
@@ -0,0 +1,19 @@
+package com.cg.set;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortSetExample {
+	public static void main(String[] args) {
+		SortedSet <String>s=new TreeSet<String> ();
+		s.add("Chess");
+		s.add("Raja");
+		s.add("Rani");
+		s.add("Allow");
+		
+		System.out.println(s);
+
+	}
+
+
+}
