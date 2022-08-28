49bce2b6197b3afae023af15299d9ab1843c4eed
Admin <Admin@DESKTOP-IJ4V5DK>
2022-08-28 15:36:13 +0530

    First Commit

---
+++ Day22_Date_time/src/com/cg/datetime/Example2.java
@@ -0,0 +1,16 @@
+package com.cg.datetime;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		String datetime="2022-04-27 18:17";
+		DateTimeFormatter df=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		LocalDateTime l=LocalDateTime.parse(datetime, df);
+		System.out.println(l);
+
+	}
+
+}
